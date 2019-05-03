e773b75812c401ad9d245d77337a6811615d14e4
Your Name <you@example.com>
2019-05-03 11:20:12 -0500

    tabs functioning,content.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((x, y)  => (
+            <Tab key={y} tab={x} selectTabHandler={props.changeSelected} selectedTab={props.selectedTab} />
+          ))}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+
+Tabs.propTypes = {
+  tabs: PropTypes.array  
+};
+
 export default Tabs;
